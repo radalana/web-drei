6b9286ab67862d3364c8a38ce8a96998e28ffcbf
Svetlana <radalana415@gmail.com>
2024-04-15 21:11:40 +0200

    Finish template for list

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 start:
-    flask --app app --debug run --port 8000
+	flask --app app --debug run --port 8000
--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 #flask --app app run --port 8000 server startet
-@app.get("/customers")
-def index():
+@app.get("/")
+def customers():
     conn = sqlite3.connect('customer_data.db')
     cursor = conn.cursor()
 
     # Получение данных из базы данных
     cursor.execute("SELECT \"Last Name\", Company, Country FROM customers")
     customers = cursor.fetchall()
-    print(customers)
 
     # Преобразование данных в список словарей для JSON ответа
     columns = [column[0] for column in cursor.description]
-    result = [dict(zip(columns, customer)) for customer in customers]
+    customers = [dict(zip(columns, customer)) for customer in customers]
 
     conn.close()
-    return render_template('index.html', list=result)
-
+    return render_template('index.html', customers=customers)
 
 if __name__ == "__main__":
   app.run()
+
