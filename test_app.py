a0bbb8a458b532a598d5c6c8450172497c8b5326
Svetlana <radalana415@gmail.com>
2024-04-17 18:40:33 +0200

    Add tests for sorting

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import pytest
+from flask import url_for
 from app import app as flask_app
 
 @pytest.fixture
@@ -6,7 +7,21 @@ def app():
     return flask_app
 
 def test_index(client):
-    response = client.get('/customers')
+    response = client.get('/customers/')
     assert response.status_code == 200
+    assert 'Delacruz-Rosario' in response.get_data(as_text=True)
 
+def test_sort_by_company(client):
+    response = client.get(url_for('index', sortBy='Company'))
+    assert response.status_code == 200
+   
+    data = response.get_data(as_text=True)
+    assert 'Arroyo, Cain and Hudson' in data  
+
+def test_sort_by_country(client):
+    response = client.get(url_for('index', sortBy='Country'))
+    assert response.status_code == 200
+   
+    data = response.get_data(as_text=True)
+    assert 'Andorra' in data
 
